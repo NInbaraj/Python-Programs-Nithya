3d5dd36dbd86301150a68997c9fc82fbe326bf22
Nithya <chem_nithya@yahoo.co.in>
2021-12-06 13:03:41 -0500

    Learning to use def for writing programs

--- Program_12.py
+++ Program_12.py
@@ -7,15 +7,15 @@ my_integer_list =[]
 
 def add_values_in_list():
     for i in range(5):
-        my_value = input('Enter the value')
+        my_value = input('Enter the value:  ')
         my_list.append(my_value)
     return my_list
 
-my_new_string = add_values_in_list(my_string_list)
+my_new_string = add_values_in_list()
 print(my_new_string)
-my_new_float = add_values_in_list(my_float_list)
+my_new_float = add_values_in_list()
 print(my_new_float)
-my_new_integer = add_values_in_list(my_integer_list)
+my_new_integer = add_values_in_list()
 print(my_new_integer)
 
 
