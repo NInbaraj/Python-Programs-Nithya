1bd720d1483fdba9f8eafd848f0b8ed5145b1e8b
Nithya <chem_nithya@yahoo.co.in>
2021-12-07 00:03:50 -0500

    Email password conf file

---
+++ config_cansr.py
@@ -0,0 +1,7 @@
+'''
+Create a conf file for Cansr application with the login details.
+This file can be imported to other programs
+'''
+
+email = 'danielhayward@yopmail.com'
+password = 'Chen@42VA'
