6ea02844f9702142bce3bb83dec0593b8543b3c6
Nithya <chem_nithya@yahoo.co.in>
2021-12-08 12:21:05 -0500

    Simple Program- calculate the salary,sum and IF

--- Program_2.py
+++ Program_2.py
@@ -1,10 +1,16 @@
-#Get 2 persons salary and calculate the sum of the salaries
-# Basic program
-#WOrk more on making this a bigger program
+'''
+Get 2 persons salary and calculate the sum of the salaries
+Basic program
+WOrk more on making this a bigger program
+'''
 person1 = int(input('Enter the salary of person 1 ='))
 person2 = int(input('Enter the salary of person 2 ='))
+
 print('The salary of Person 1 and Person 2 is', person1, person2)
+
 sum = int(person1) + int(person2)
+
 print('The total salary is', sum)
+
 if sum>2500:
     print('It is a good salary')
