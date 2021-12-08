a8431dbfcebede33dbc8affa08f3ec6589456261
Nithya <chem_nithya@yahoo.co.in>
2021-12-08 12:16:14 -0500

    1st Program-Calculate BMI IF & ELIF

--- Program_1.py
+++ Program_1.py
@@ -1,24 +1,33 @@
-
-#Calculate the BMI of a person
-#Give appropriate message based on BMI
-#Calculate the BMI of a Person
+'''
+My First Python program
+Calculate the BMI of a Person
+#Give the appropriate message for BMI values
+'''
 
 height = float(input('Your height in inches: '))
 weight = float(input('Your weight in pounds: '))
+
 bmi = float(weight/(height*height))*703
+
 if bmi <= 16.0:
     print('Your BMI is: ', bmi, 'You are Severely Underweight')
+
 elif bmi <= 18.4:
     print('Your BMI is: ', bmi, 'You are Underweight')
+
 elif bmi <= 24.9:
     print('Your BMI is: ', bmi, 'You are Normal')
+
 elif bmi <= 29.9:
     print('Your BMI is: ', bmi, 'You are Overweight')
+
 elif bmi <= 34.9:
     print('Your BMI is: ', bmi, 'You are Moderately Obese')
+
 elif bmi <= 39.9:
     print('Your BMI is: ', bmi, 'You are Severely Obese')
-elif bmi > 39.9:
+    
+elif bmi >= 40:
     print('Your BMI is: ', bmi, 'You are Morbidly Obese')
     
 
